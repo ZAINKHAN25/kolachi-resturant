4832deaf217613af1195bb4d5a72ead5a56ee347
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-23 17:31:03 +0500

    Sub Landing Page Done.

--- src/app/components/SubLandingSection/index.jsx
+++ src/app/components/SubLandingSection/index.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+const dummyData = ["Brak Fast", "Main Dishes", "Drinks", "Desserts"];
+
 function SubLandingSection() {
     return (
-        <div>
-            SubLandingSection
+        <div className='Sub-Landing-Page-Section'>
+            <h1 className='text-6xl'>
+                Browse Our Menu
+            </h1>
+            <div className='menu-items'>
+                {dummyData.map((item, index) => (
+                    <div className='menu-item' key={index}>
+                        <h2>{item}</h2>
+                        <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default SubLandingSection;
