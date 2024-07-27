a3a038f00b061bf980e8cc36fed0bde33ca50004
ZAIN-KHAN-25 <zainarfeen87@gmail.com>
2024-07-27 19:57:29 +0500

    Some improvements.

--- src/app/components/SubLandingSection/index.jsx
+++ src/app/components/SubLandingSection/index.jsx
@@ -1,21 +1,57 @@
 import React from 'react';
+import { PiCoffeeFill } from "react-icons/pi";
+import { LuPopcorn } from "react-icons/lu";
+import { CiGlass } from "react-icons/ci";
+import { GiCakeSlice } from "react-icons/gi";
 
-const dummyData = ["Brak Fast", "Main Dishes", "Drinks", "Desserts"];
+const dummyData = [
+    {
+        name: "Brak Fast",
+        icon: <PiCoffeeFill />
+    },
+    {
+        name: "Main Dishes",
+        icon: <LuPopcorn />
+    },
+    {
+        name: "Drinks",
+        icon: <CiGlass />
+    },
+    {
+        name: "Desserts",
+        icon: <GiCakeSlice />
+    }
+];
 
 function SubLandingSection() {
     return (
-        <div className='Sub-Landing-Page-Section'>
-            <h1 className='text-6xl'>
-                Browse Our Menu
-            </h1>
-            <div className='menu-items'>
-                {dummyData.map((item, index) => (
-                    <div className='menu-item' key={index}>
-                        <h2>{item}</h2>
-                        <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
-                    </div>
-                ))}
+        <div
+            className='Sub-Landing-Page-Section'
+        >
+            <div
+                className='container mx-auto'
+            >
+
+                <h1
+                    className='text-6xl'
+                >
+                    Browse Our Menu
+                </h1>
+                <div
+                    className='menu-items'
+                >
+                    {
+                        dummyData.map((item, index) => (
+                            <div className='menu-item' key={index}>
+                                <h2>{item.name}</h2>
+                                <span>{item.icon}</span>
+                                <p>In the new era of technology we look in the future with certainty and pride for our life.</p>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
+
         </div>
     );
 };
